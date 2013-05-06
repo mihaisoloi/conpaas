0623d3ea0c18d227310dc03cec963193dee69e68
Mihai Soloi <mihai.soloi@gmail.com>
2013-05-06 19:03:49 +0200

    added the php pages for the service

---
+++ conpaas-frontend/www/lib/service/faulttolerance/__init__.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * Copyright (c) 2010-2012, Contrail consortium.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the
+ *     above copyright notice, this list of conditions
+ *     and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce
+ *     the above copyright notice, this list of
+ *     conditions and the following disclaimer in the
+ *     documentation and/or other materials provided
+ *     with the distribution.
+ *  3. Neither the name of the Contrail consortium nor the
+ *     names of its contributors may be used to endorse
+ *     or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_module('cloud');
+require_module('service');
+require_module('ui/instance/faulttolerance');
+
+class FaultToleranceService extends Service {
+
+	public function __construct($data, $manager) {
+		parent::__construct($data, $manager);
+	}
+
+	public function hasDedicatedManager() {
+		return true;
+	}
+
+	public function sendConfiguration($params){
+		return '{}';
+	}
+
+	public function fetchHighLevelMonitoringInfo() {
+		return false;
+	}
+
+	public function getInstanceRoles() {
+		return array('dir', 'mrc','osd');
+	}
+
+	public function fetchStateLog() {
+		return array();
+	}
+
+	public function needsPolling() {
+		return parent::needsPolling() || $this->state == self::STATE_INIT;
+	}
+
+	public function createInstanceUI($node) {
+		$info = $this->getNodeInfo($node);
+		return new FaultToleranceInstance($info);
+	}
+
+	public function createVolume($volumeName,$owner) {
+		$resp = $this->managerRequest('post','createVolume',array(
+                'volumeName' => $volumeName,
+                'owner' => $owner));
+		return $resp;
+	}
+	public function deleteVolume($volumeName) {
+		$resp = $this->managerRequest('post','deleteVolume',array(
+				'volumeName' => $volumeName));
+		return $resp;
+	}
+
+ 	public function viewVolumes() {
+ 		$json = $this->managerRequest('get', 'viewVolumes', array());
+ 		$volumes = json_decode($json, true);
+ 		return $volumes['result']['volumes'];
+ 	}
+}
+
+?>
