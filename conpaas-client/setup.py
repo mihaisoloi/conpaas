mihaisoloi/conpaas

commit 8354a384fa769264533580b1d5fbf4d2028a4879

conpaas: remove text of BSD license.
git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/contrail/trunk/conpaas@4999 ef35cc4a-2640-4bbf-98bc-65920db7d8ae

conpaas-client/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-CPSVERSION = '1.2.0'
+CPSVERSION = '1.2.1'
 
 long_description = """
 ConPaaS: an integrated runtime environment for elastic Cloud applications 

conpaas-director/setup.py
@@ -9,7 +9,7 @@ from grp import getgrnam
 from setuptools import setup
 from pkg_resources import Requirement, resource_filename
 
-CPSVERSION = '1.2.0'
+CPSVERSION = '1.2.1'
 
 CONFDIR = '/etc/cpsdirector'
 

conpaas-services/src/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command import sdist
 del sdist.finders[:]
 
-CPSVERSION = '1.2.0'
+CPSVERSION = '1.2.1'
 
 long_description = """
 ConPaaS: an integrated runtime environment for elastic Cloud applications 

conpaas-services/src/conpaas/core/agent.py
@@ -1,38 +1,12 @@
+# -*- coding: utf-8 -*-
+
 """
-Copyright (c) 2010-2013, Contrail consortium.
-All rights reserved.
+    conpaas.core.agent
+    ====================
 
-Redistribution and use in source and binary forms, 
-with or without modification, are permitted provided
-that the following conditions are met:
+    ConPaaS core: service-independent agent code.
 
- 1. Redistributions of source code must retain the
-    above copyright notice, this list of conditions
-    and the following disclaimer.
- 2. Redistributions in binary form must reproduce
-    the above copyright notice, this list of 
-    conditions and the following disclaimer in the
-    documentation and/or other materials provided
-    with the distribution.
- 3. Neither the name of the Contrail consortium nor the
-    names of its contributors may be used to endorse
-    or promote products derived from this software 
-    without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+    :copyright: (C) 2010-2013 by Contrail Consortium.
 """
 
 from conpaas.core.expose import expose

conpaas-services/src/conpaas/core/clouds/base.py
@@ -1,43 +1,13 @@
-'''
-Copyright (c) 2010-2012, Contrail consortium.
-All rights reserved.
+# -*- coding: utf-8 -*-
 
-Redistribution and use in source and binary forms,
-with or without modification, are permitted provided
-that the following conditions are met:
+"""
+    conpaas.core.clouds.base
+    ========================
 
- 1. Redistributions of source code must retain the
-    above copyright notice, this list of conditions
-    and the following disclaimer.
- 2. Redistributions in binary form must reproduce
-    the above copyright notice, this list of
-    conditions and the following disclaimer in the
-    documentation and/or other materials provided
-    with the distribution.
- 3. Neither the name of the Contrail consortium nor the
-    names of its contributors may be used to endorse
-    or promote products derived from this software
-    without specific prior written permission.
+    ConPaaS core: cloud-independent IaaS code.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-Created on Jan 25, 2012
-
-@file
-'''
+    :copyright: (C) 2010-2013 by Contrail Consortium.
+"""
 
 from conpaas.core.node import ServiceNode
 

conpaas-services/src/conpaas/core/clouds/dummy.py
@@ -1,39 +1,13 @@
-'''
-Copyright (c) 2010-2012, Contrail consortium.
-All rights reserved.
+# -*- coding: utf-8 -*-
 
-Redistribution and use in source and binary forms,
-with or without modification, are permitted provided
-that the following conditions are met:
+"""
+    conpaas.core.clouds.dummy
+    =========================
 
- 1. Redistributions of source code must retain the
-    above copyright notice, this list of conditions
-    and the following disclaimer.
- 2. Redistributions in binary form must reproduce
-    the above copyright notice, this list of
-    conditions and the following disclaimer in the
-    documentation and/or other materials provided
-    with the distribution.
- 3. Neither the name of the Contrail consortium nor the
-    names of its contributors may be used to endorse
-    or promote products derived from this software
-    without specific prior written permission.
+    ConPaaS core: Dummy cloud IaaS code.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-'''
+    :copyright: (C) 2010-2013 by Contrail Consortium.
+"""
 
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver

conpaas-services/src/conpaas/core/clouds/ec2.py
@@ -1,39 +1,13 @@
-'''
-Copyright (c) 2010-2012, Contrail consortium.
-All rights reserved.
+# -*- coding: utf-8 -*-
 
-Redistribution and use in source and binary forms,
-with or without modification, are permitted provided
-that the following conditions are met:
+"""
+    conpaas.core.clouds.ec2
+    =======================
 
- 1. Redistributions of source code must retain the
-    above copyright notice, this list of conditions
-    and the following disclaimer.
- 2. Redistributions in binary form must reproduce
-    the above copyright notice, this list of
-    conditions and the following disclaimer in the
-    documentation and/or other materials provided
-    with the distribution.
- 3. Neither the name of the Contrail consortium nor the
-    names of its contributors may be used to endorse
-    or promote products derived from this software
-    without specific prior written permission.
+    ConPaaS core: Amazon EC2 IaaS code.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-'''
+    :copyright: (C) 2010-2013 by Contrail Consortium.
+"""
 
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver

conpaas-services/src/conpaas/core/clouds/opennebula.py
@@ -1,38 +1,12 @@
+# -*- coding: utf-8 -*-
+
 """
-Copyright (c) 2010-2012, Contrail consortium.
-All rights reserved.
+    conpaas.core.clouds.opennebula
+    ==============================
 
-Redistribution and use in source and binary forms,
-with or without modification, are permitted provided
-that the following conditions are met:
+    ConPaaS core: OpenNebula IaaS code.
 
- 1. Redistributions of source code must retain the
-    above copyright notice, this list of conditions
-    and the following disclaimer.
- 2. Redistributions in binary form must reproduce
-    the above copyright notice, this list of
-    conditions and the following disclaimer in the
-    documentation and/or other materials provided
-    with the distribution.
- 3. Neither the name of the Contrail consortium nor the
-    names of its contributors may be used to endorse
-    or promote products derived from this software
-    without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+    :copyright: (C) 2010-2013 by Contrail Consortium.
 """
 
 import urlparse

commit 940a704932651b404de8e85390a7756bd7359021

conpaas: revert version bumping wrongly introduced in r4999
git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/contrail/trunk/conpaas@5001 ef35cc4a-2640-4bbf-98bc-65920db7d8ae

conpaas-client/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-CPSVERSION = '1.2.1'
+CPSVERSION = '1.2.0'
 
 long_description = """
 ConPaaS: an integrated runtime environment for elastic Cloud applications 

conpaas-director/setup.py
@@ -9,7 +9,7 @@ from grp import getgrnam
 from setuptools import setup
 from pkg_resources import Requirement, resource_filename
 
-CPSVERSION = '1.2.1'
+CPSVERSION = '1.2.0'
 
 CONFDIR = '/etc/cpsdirector'
 

conpaas-services/src/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command import sdist
 del sdist.finders[:]
 
-CPSVERSION = '1.2.1'
+CPSVERSION = '1.2.0'
 
 long_description = """
 ConPaaS: an integrated runtime environment for elastic Cloud applications 